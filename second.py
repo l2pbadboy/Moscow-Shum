216d31b93db72e2c3ee9f5b853275c358a38411e
user <list0000g@gmail.com>
2024-12-04 19:49:36 +0300

    AAA

---
+++ second.py
@@ -0,0 +1,36 @@
+import sys
+import random
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtCore import Qt
+
+
+class CircleWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Рисование круга")
+        self.setGeometry(100, 100, 400, 400)
+
+        self.button = QPushButton("Нарисовать круг", self)
+        self.button.clicked.connect(self.draw_circle)
+        self.button.setGeometry(10, 380, 380, 20)
+
+        self.diameter = 0
+
+
+    def draw_circle(self):
+        self.diameter = random.randint(0, 400)
+        self.update()
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setBrush(QColor(255, 255, 0))
+        x = (self.width() - self.diameter) // 2
+        y = (self.height() - self.diameter) // 2
+        painter.drawEllipse(x, y, self.diameter, self.diameter)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = CircleWidget()
+    window.show()
+    sys.exit(app.exec())
